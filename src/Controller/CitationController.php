6311eda341a6af3bdee8c5915e0ce88c9705cd97
Gabriel <rougegabriel2@gmail.com>
2025-02-10 11:05:02 +0100

    Amélioration du style

--- src/Controller/CitationController.php
+++ src/Controller/CitationController.php
@@ -4,21 +4,36 @@ namespace App\Controller;
 
 use App\Api\CitationApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CitationController extends AbstractController
 {
     private CitationApi $citationApi;
-    public function __construct(){
-        $this->citationApi = new CitationApi(HttpClient::create(),$_ENV['API_BASE_URL']);
+
+    public function __construct(CitationApi $citationApi)
+    {
+        $this->citationApi = $citationApi;
     }
+
     #[Route('/', name: 'app_citation')]
     public function index(): Response
     {
+        try {
+            $citation = $this->citationApi->getRandomCitation();
+        } catch (\Exception $e) {
+            $this->addFlash('danger', 'Impossible de récupérer une citation pour le moment.');
+            return $this->redirectToRoute('app_citation'); // Recharge la page en cas d'erreur
+        }
+
         return $this->render('citation/index.html.twig', [
-            'citation' => $this->citationApi->getRandomCitation(),
+            'citation' => $citation,
         ]);
+    }
+
+    #[Route('/random', name: 'random_citation')]
+    public function random(): Response
+    {
+        return $this->redirectToRoute('app_citation'); // Recharge la page pour afficher une nouvelle citation
     }
 }
